553c74a1755a0f3d3fcb733ea1fa988f40fa1101
Daniel Pacheco <danielalejandropaba@gmail.com>
2026-03-19 19:56:24 -0500

    I learn about loops and control flow code

--- test.py
+++ test.py
@@ -1,13 +1,59 @@
-my_age = 21
+statement_one = False
 
-half_my_age = my_age / 2
+statement_two = True
 
-greeting = "Hola "
+credits = 120
+gpa = 1.8
 
-name = "Daniel"
+if not credits >= 120:
+    print("You do not have enough credits to graduate")
+    
+if not gpa >= 2.0:
+    print("Your GPA is not high enough to graduate")
+    
+if not credits >= 120 and not gpa >= 2.0:
+    "You do not meet either requierements to graduate"
+    
+grade = 86
 
-greeting_with_name = greeting + name
+if grade >= 90:
+    print("A")
+elif grade >= 80:
+    print("B")
+elif grade >= 70:
+    print("C")
+elif grade >= 60:
+    print("D")
+else:
+    print("F")
+    
+    # 1. Definimos el peso en la Tierra y el número del planeta
+peso_tierra = 70  # Puedes cambiar este número
+numero_planeta = 3 # Digamos que Codey va a Jupiter
 
+# 2. Empezamos la "escalera" de decisiones
+if numero_planeta == 1:
+    peso_destino = peso_tierra * 0.91
+    nombre_planeta = "Venus"
+elif numero_planeta == 2:
+    peso_destino = peso_tierra * 0.38
+    nombre_planeta = "Mars"
+elif numero_planeta == 3:
+    peso_destino = peso_tierra * 2.34
+    nombre_planeta = "Jupiter"
+elif numero_planeta == 4:
+    peso_destino = peso_tierra * 1.06
+    nombre_planeta = "Saturn"
+elif numero_planeta == 5:
+    peso_destino = peso_tierra * 0.92
+    nombre_planeta = "Uranus"
+elif numero_planeta == 6:
+    peso_destino = peso_tierra * 1.19
+    nombre_planeta = "Neptune"
+else:
+    print("Ese número de planeta no existe en nuestra lista.")
+    peso_destino = None
 
-print(half_my_age)
-print(greeting_with_name)
+# 3. Mostramos el resultado si el planeta era válido
+if peso_destino:
+    print(f"Tu peso en {nombre_planeta} sería de {peso_destino} kg.")
